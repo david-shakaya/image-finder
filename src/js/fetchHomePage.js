a3fffc5996424d4647a296c2f4bfda19dc680b81
david-shakaya <devidoz666@gmail.com>
2021-02-04 16:24:40 +0200

    рендер главной страницы

--- src/js/fetchHomePage.js
+++ src/js/fetchHomePage.js
@@ -1,14 +1,20 @@
 // editors_choice
+import refs from './refs'
+import galleryHomePage from '../templates/gallery-template.hbs'
 
 
 const KEY = '14396786-a714bdf8d854f524afdc45598';
 const perPage = 12
 let page = 1
-export default function fetchHomePage(query) {
-    // queryForPageTwo = query
+export default function fetchHomePage() {
     page = 1
-    // isEndPage = false
     fetch(`https://pixabay.com/api/?key=${KEY}&image_type=photo&orientation=horizontal&page=${page}&per_page=${perPage}&editors_choice=true`)
         .then(response => response.json())
-    .then(data => console.log(data))
+        .then(data => {
+    
+            const markup = galleryHomePage(data.hits)
+            refs.ulGallery.insertAdjacentHTML('beforeend', markup)
+            refs.hideSpiner.classList.remove('loader')
+        })
+           
 }
