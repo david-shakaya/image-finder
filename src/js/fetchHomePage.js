53da2b5fc40496f966e93f22644f82cd2022d4fb
david-shakaya <devidoz666@gmail.com>
2021-02-06 16:12:06 +0200

    Добавляет постраничную пагинацию

--- src/js/fetchHomePage.js
+++ src/js/fetchHomePage.js
@@ -2,15 +2,18 @@
 import refs from './refs'
 import galleryHomePage from '../templates/gallery-template-home-page.hbs'
 
-
-const KEY = '14396786-a714bdf8d854f524afdc45598';
-const perPage = 15
 let page = 1
+
+function getFetch(page) {
+   const KEY = '14396786-a714bdf8d854f524afdc45598';
+   return fetch(`https://pixabay.com/api/?key=${KEY}&image_type=photo&orientation=horizontal&page=${page}&per_page=15&editors_choice=true`)
+       .then(response => response.json())
+}
+
+
 export default function fetchHomePage() {
     page = 1
-    fetch(`https://pixabay.com/api/?key=${KEY}&image_type=photo&orientation=horizontal&page=${page}&per_page=${perPage}&editors_choice=true`)
-        .then(response => response.json())
-        .then(data => {
+   getFetch().then(data => {
             console.log(data);
             refs.galeryTitle.textContent = 'Editors Choice - Stunning images selected by our team'
             refs.galeryTitle.classList.remove('error')
@@ -18,25 +21,33 @@ export default function fetchHomePage() {
             const markup = galleryHomePage(data.hits)
             refs.ulGallery.insertAdjacentHTML('beforeend', markup)
             refs.hideSpiner.classList.remove('loader')
-        })
-           
-}
 
-var items = $(".list-wrapper .list-item");
+
+
+    var items = $(".list-wrapper .list-item");
     var numItems = items.length;
-    var perPage = 4;
+    var perPages = 15;
 
-    items.slice(perPage).hide();
+    items.slice(perPages).hide();
 
+       console.log(window.location.href);
+       
     $('#pagination-container').pagination({
-        items: numItems,
-        itemsOnPage: perPage,
+        items: `${data.totalHits}`,
+        itemsOnPage: perPages,
         prevText: "&laquo;",
         nextText: "&raquo;",
         onPageClick: function (pageNumber) {
-            var showFrom = perPage * (pageNumber - 1);
-            var showTo = showFrom + perPage;
-            items.hide().slice(showFrom, showTo).show();
+            getFetch(pageNumber).then(data => {
+            const markup = galleryHomePage(data.hits)
+            refs.ulGallery.innerHTML =  markup
+            })
         }
     });
+       })
+           
+}
+
+
+
 
